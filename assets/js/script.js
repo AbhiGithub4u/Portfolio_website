66c20ac7fd7a16272ad88fa78bd53352a6582a01
@AbhiGit4u <abhishekgupta60414@gmail.com>
2023-01-25 20:06:58 +0530

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+let ham = document.getElementById("hamburger")
+let navbars = document.getElementById("navigation")
+
+ham.addEventListener('click', () => {
+    navbars.classList.toggle('show')
+
+    
+
+});
