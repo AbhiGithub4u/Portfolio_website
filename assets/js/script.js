4676d0d2d8d53ff4210413c5ee2f42ad7fdb2c88
Abbhishek Gupta <Abhishekgupta60414@gmail.com>
2023-01-26 15:48:16 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ let navbars = document.getElementById("navigation")
 ham.addEventListener('click', () => {
     navbars.classList.toggle('show')
 
-    
-
 });
+
+window.onscroll=()=>{
+    navbars.classList.remove("show")
+
+
+};
